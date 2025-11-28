6a7f52d49bcbde02b711d90dca688998ac92f3f9
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2025-11-29 02:43:43 +0545

    Initial Commit

--- kitaab/obj/Debug/net9.0/kitaab.AssemblyInfo.cs
+++ kitaab/obj/Debug/net9.0/kitaab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kitaab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49749f67d03df22bbd2c1ac90a04792056117dc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7963ccb60249cd9f029395f60483353afe04a57")]
 [assembly: System.Reflection.AssemblyProductAttribute("kitaab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kitaab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
